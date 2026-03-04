1fe5bbc788c6569435b28bdae11023c538509180
martinljor <martinljor@users.noreply.github.com>
2026-03-04 11:54:08 +0000

    Add K10 snapshot-class annotation to Longhorn VolumeSnapshotClass

--- steps/08-longhorn.sh
+++ steps/08-longhorn.sh
@@ -133,6 +133,7 @@ metadata:
   name: longhorn-snapshot-vsc
   annotations:
     snapshot.storage.kubernetes.io/is-default-class: 'true'
+    k10.kasten.io/is-snapshot-class: 'true'
 driver: driver.longhorn.io
 deletionPolicy: Delete
 EOF
